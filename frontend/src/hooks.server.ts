04143199b8579106ef60b64b4dd5b68e7ba6a799
Sebastian Hauschildt <sebastian@hauschildt.dev>
2025-10-15 00:13:37 +0200

    packages + token fix

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -5,6 +5,8 @@ import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { cors } from './lib/server/hooks/cors';
 import { csrf } from './lib/server/hooks/csrf';
+import * as jose from 'jose';
+import { match, P } from 'ts-pattern';
 
 const handleParaglide: Handle = i18n.handle();
 
@@ -13,16 +15,26 @@ export const handleAuth: Handle = ({ event, resolve }) => {
 		return resolve(event);
 	}
 	const token = event.cookies.get('auth_token');
-	if (token) {
-		client.interceptors.request.use((request) => {
-			request.headers.set('Authorization', `Bearer ${token}`);
-			return request;
-		});
-		if (event.route.id?.includes('auth')) {
-			throw redirect(303, '/');
-		}
-	} else if (!event.route.id?.includes('auth')) {
+	
+	
+	if (!token && !event.route.id?.includes('auth')) {
 		return redirect(303, '/auth/login');
+	}
+	
+	try {
+		jose.decodeJwt(token ?? '');
+	} catch (error) {
+		event.cookies.delete('auth_token', { path: '/' });
+		return resolve(event);
+	}
+
+	client.interceptors.request.use((request) => {
+		request.headers.set('Authorization', `Bearer ${token}`);
+		return request;
+	});
+
+	if (event.route.id?.includes('auth')) {
+		throw redirect(303, '/');
 	}
 
 	return resolve(event);
